d6cec30397015fda8f8e619c85c8d9a61d17c8ea
hieu-letrong-biz <hieu.letrong@bizreach.co.jp>
2020-04-23 10:54:14 +0900

    TODO Step02 Done

--- src/test/java/org/docksidestage/javatry/basic/Step03DataTypeTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step03DataTypeTest.java
@@ -84,9 +84,9 @@ public class Step03DataTypeTest extends PlainTestCase {
             sea = 0;
         }
         log(sea); // your answer? => 2,cause (byte) amba =2
-        // TODO Do you knoq why dstore > piari is false? winkichanwi
-        //Comparing a float with integer is internally taken to be comparison between two floats (int will be casted to float).
-
+        // TODO done Do you knoq why dstore > piari is false? winkichanwi
+        // TODO answer. Comparing a float with integer is internally taken to be
+        //  comparison between two floats (int will be casted to float).
     }
 
     // ===================================================================================
